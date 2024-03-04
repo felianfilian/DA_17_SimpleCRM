20e909354e8ec90e48327bd533b9e46d1189de3f
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-04 12:20:34 +0100

    show routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
-  { path: '/', component: DashboardComponent },
-  { path: '/dashboard', component: DashboardComponent },
-  { path: '/user', component: UserComponent },
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'user', component: UserComponent },
 ];
